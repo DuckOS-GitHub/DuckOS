d5d8e89265a7addcea0564dffdc33df1e37095d3
AnhNguyenlost13 <94160753+AnhNguyenlost13@users.noreply.github.com>
2022-09-20 10:05:33 +0700

    wtf
    i lost my brain cells editing this file

--- src/ProgramData/Cache_Cleaner.bat
+++ src/ProgramData/Cache_Cleaner.bat
@@ -1,12 +1,7 @@
-del /s /f /q c:\windows\temp\*.*
-del /s /f /q C:\WINDOWS\Prefetch\*.*
+del /s /f /q c:\Windows\temp\*.*
+del /s /f /q C:\Windows\prefetch\*.*
 del /s /f /q %temp%\*.*
-del /f /q c:\windows\tempor~1
-del /f /q c:\windows\temp
-del /f /q c:\windows\tmp
-del /f /q c:\windows\ff*.tmp
-del /f /q c:\windows\history
-del /f /q c:\windows\cookies
-del /f /q c:\windows\recent
-del /f /q c:\windows\spool\printers
-del *.log /a /s /q /f
+del /s /f /q c:\windows\temp
+del /s /f /q c:\windows\*.tmp
+cd /d C:\
+del *.log /s /q /f
