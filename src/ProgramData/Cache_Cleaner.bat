494332ed5f7e57074913bf385cfa2708d0d4f195
AnhNguyenlost13 <94160753+AnhNguyenlost13@users.noreply.github.com>
2022-09-20 14:41:39 +0700

    progress indicator

--- src/ProgramData/Cache_Cleaner.bat
+++ src/ProgramData/Cache_Cleaner.bat
@@ -1,7 +1,12 @@
+@echo off
+title DuckOS - Cache Cleaner
+echo Deleting Windows temporary files...
 del /s /f /q c:\Windows\temp\*.*
+echo Deleting prefetch files....
 del /s /f /q C:\Windows\prefetch\*.*
+echo Deleting temporary folder...
 del /s /f /q %temp%\*.*
-del /s /f /q c:\windows\temp
-del /s /f /q c:\windows\*.tmp
 cd /d C:\
+echo Removing all logs and other temporary files...
 del *.log /s /q /f
+del *.tmp /s /q /f
