995ea8f2ce78bc03d0f219a3ab030c510ec155e6
AnhNguyenlost13 <94160753+AnhNguyenlost13@users.noreply.github.com>
2022-11-26 22:11:00 +0700

    Update Reinstall Applications.bat

--- src/Desktop/DuckOS - Post Install Folder/Troubleshooting/Re-Install applications/Reinstall Applications.bat
+++ src/Desktop/DuckOS - Post Install Folder/Troubleshooting/Re-Install applications/Reinstall Applications.bat
@@ -1,24 +1,35 @@
 @echo off && cls && echo ! Loaded.
+@set c=0
 echo.
-echo info: this will re-install all DuckOS' preinstalled apps
-echo - 7-Zip
-echo - Open-Shell
-echo - Visual C++ Redists
+echo [ INFO ] This script will re-install all DuckOS' preinstalled apps:
+echo [ INFO ] - 7-Zip
+echo [ INFO ] - Open-Shell
+echo [ INFO ] - Visual C++ Redists
 pause
 if exist C:\Windows\Setup\Files\7zip.msi (
-	echo info: 7-Zip  installer found - installing
-	"C:\Windows\Setup\Files\7zip.msi" /quiet
-	echo info: Done.
+    echo [ INFO ] 7-Zip installer found - installing
+    "C:\Windows\Setup\Files\7zip.msi" /quiet
+    set /a c=c+1
+    echo [ INFO ] Done.
+) else (
+    echo [ WARN ] 7-Zip installer not found - skipping
 )
+
 if exist C:\Windows\Setup\Files\vcredist.exe (
-	echo info: Visual C++ Redists installer found - installing
-	"C:\Windows\Setup\Files\vcredist.exe" /ai
-	echo info: Done.
+    echo [ INFO ] Visual C++ Redistributables installer found - installing
+    set /a c=c+1
+    "C:\Windows\Setup\Files\vcredist.exe" /ai
+    echo [ INFO ] Done.
+) else (
+    echo [ WARN ] Visual C++ Redistributables installer not found - skipping
 )
 if exist C:\Windows\Setup\Files\openshell.exe (
-	echo info: Open-Shell installer found - installing
-	"C:\Windows\Setup\Files\openshell.exe" /qn ADDLOCAL=StartMenu
-	echo info: Done.
+    echo [ INFO ] Open-Shell installer found - installing
+    set /a c=c+1
+    "C:\Windows\Setup\Files\openshell.exe" /qn ADDLOCAL=StartMenu
+    echo [ INFO ] Done.
+) else (
+    echo [ WARN ] Open-Shell installer not found - skipping
 )
-echo info: press any key to exit
+echo [ INFO ] echo %c%/3 of the installers were executed. Press any key to exit
 pause > nul & exit /b 1
