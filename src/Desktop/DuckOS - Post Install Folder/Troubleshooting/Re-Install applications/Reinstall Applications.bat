3a5f66363d940b1d410d343c028ee40ac9c2959e
AnhNguyenlost13 <94160753+AnhNguyenlost13@users.noreply.github.com>
2022-11-27 15:56:20 +0700

    remove extra space before/after brackets
    only 4 chars anyway

--- src/Desktop/DuckOS - Post Install Folder/Troubleshooting/Re-Install applications/Reinstall Applications.bat
+++ src/Desktop/DuckOS - Post Install Folder/Troubleshooting/Re-Install applications/Reinstall Applications.bat
@@ -1,35 +1,35 @@
 @echo off && cls && echo ! Loaded.
 @set c=0
 echo.
-echo [ INFO ] This script will re-install all DuckOS' preinstalled apps:
-echo [ INFO ] - 7-Zip
-echo [ INFO ] - Open-Shell
-echo [ INFO ] - Visual C++ Redists
+echo [INFO] This script will re-install all DuckOS' preinstalled apps:
+echo [INFO] - 7-Zip
+echo [INFO] - Open-Shell
+echo [INFO] - Visual C++ Redists
 pause
 if exist C:\Windows\Setup\Files\7zip.msi (
-    echo [ INFO ] 7-Zip installer found - installing
+    echo [INFO] 7-Zip installer found - installing
     "C:\Windows\Setup\Files\7zip.msi" /quiet
     set /a c=c+1
-    echo [ INFO ] Done.
+    echo [INFO] Done.
 ) else (
-    echo [ WARN ] 7-Zip installer not found - skipping
+    echo [WARN] 7-Zip installer not found - skipping
 )
 
 if exist C:\Windows\Setup\Files\vcredist.exe (
-    echo [ INFO ] Visual C++ Redistributables installer found - installing
+    echo [INFO] Visual C++ Redistributables installer found - installing
     set /a c=c+1
     "C:\Windows\Setup\Files\vcredist.exe" /ai
-    echo [ INFO ] Done.
+    echo [INFO] Done.
 ) else (
-    echo [ WARN ] Visual C++ Redistributables installer not found - skipping
+    echo [WARN] Visual C++ Redistributables installer not found - skipping
 )
 if exist C:\Windows\Setup\Files\openshell.exe (
-    echo [ INFO ] Open-Shell installer found - installing
+    echo [INFO] Open-Shell installer found - installing
     set /a c=c+1
     "C:\Windows\Setup\Files\openshell.exe" /qn ADDLOCAL=StartMenu
-    echo [ INFO ] Done.
+    echo [INFO] Done.
 ) else (
-    echo [ WARN ] Open-Shell installer not found - skipping
+    echo [WARN] Open-Shell installer not found - skipping
 )
-echo [ INFO ] echo %c%/3 of the installers were executed. Press any key to exit
+echo [INFO] echo %c%/3 of the installers were executed. Press any key to exit
 pause > nul & exit /b 1
